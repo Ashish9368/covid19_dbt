9d4f8ecba55e1cc37798dbf9f329f498dacbdacc
ashish.bhosale9368@outlook.com <ashish.bhosale9368@outlook.com>
2022-02-06 11:48:20 +0000

    Raw data collection

---
+++ models/transformation/covid_indo_raw.sql
@@ -0,0 +1,17 @@
+
+{{ config(materialized='table') }}
+
+select 
+cast(Date as date) as dt,
+week(cast(Date as date)) as wk,
+month(cast(Date as date)) as mon,
+year(cast(Date as date)) as yr,
+New_Cases as new_cases,
+New_Deaths as new_deaths,
+New_Recovered as new_recovered,
+New_Active_Cases as active_cases,
+(select Total_Cases from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_ASHISH_BHOSALEE" order by date desc limit 1) as total_cases,
+(select Total_Deaths from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_ASHISH_BHOSALEE" order by date desc limit 1) as total_deaths,
+(select Total_Recovered from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_ASHISH_BHOSALEE" order by date desc limit 1) as total_recovered,
+(select Total_Active_Cases from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_ASHISH_BHOSALEE" order by date desc limit 1) as total_active_cases
+from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_ASHISH_BHOSALEE"
